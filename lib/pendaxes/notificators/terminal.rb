bca2d1eb4da766d29b5340d507bb6d49f606d685
Shota Fukumori <sorah@cookpad.com>
2012-09-21 14:49:37 +0900

    terminal notificator

--- lib/pendaxes/notificators/terminal.rb
+++ lib/pendaxes/notificators/terminal.rb
@@ -1,6 +1,18 @@
 module Pendaxes
   class Notificator
     class Terminal < Notificator
+      defaults to: $stdout
+
+      def notify
+        io = @config.to
+        pendings.group_by{|x| "#{x[:commit][:name]} <#{x[:commit][:email]}>" }.each do |name, pends|
+          io.puts "#{name}:"
+          pends.sort_by {|x| -(Time.now - x[:commit][:at]) }.each do |pending|
+            io.puts "  #{pending[:example][:file]}:#{pending[:example][:line]} - #{pending[:example][:message]} (@ #{pending[:commit][:sha]} #{pending[:commit][:at]})"
+          end
+          io.puts
+        end
+      end
     end
   end
 end
--- spec/notificators/terminal_spec.rb
+++ spec/notificators/terminal_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../spec_helper'
 require 'pendaxes/notificators/terminal'
+require 'stringio'
 
 describe Pendaxes::Notificator::Terminal do
   it "has Pendaxes::Notificator as superclass" do
@@ -7,6 +8,56 @@ describe Pendaxes::Notificator::Terminal do
   end
 
   describe "#notify" do
-    it "notifies into terminal"
+    let(:io) { StringIO.new }
+    let(:config) { {to: io} }
+    let(:now) { Time.now }
+    let(:fixture) do
+      [
+        {commit:  {sha: 'a', name: 'foo', email: 'foo@example.com', at: (now-86400)},
+         example: {file: 'a_spec.rb', line: 10, message: "pending 'because it fails'"}, allowed: true},
+        {commit:  {sha: 'b', name: 'foo', email: 'foo@example.com', at: (now-864000)},
+         example: {file: 'a_spec.rb', line: 15, message: "pending 'because it fails.'"}, allowed: false},
+        {commit:  {sha: 'c', name: 'bar', email: 'bar@example.com', at: (now-86400)},
+         example: {file: 'a_spec.rb', line: 20, message: "pending 'because it fails..'"}, allowed: true},
+        {commit:  {sha: 'd', name: 'bar', email: 'bar@example.com', at: (now-864000)},
+         example: {file: 'a_spec.rb', line: 25, message: "pending 'because it fails...'"}, allowed: false},
+      ]
+    end
+
+    subject { described_class.new(config) }
+
+    before do
+      subject.add(fixture)
+      subject.notify
+    end
+
+    it "notifies into terminal" do
+      out = io.string
+      out.should == <<-EOF
+foo <foo@example.com>:
+  a_spec.rb:15 - pending 'because it fails.' (@ b #{now-864000})
+
+bar <bar@example.com>:
+  a_spec.rb:25 - pending 'because it fails...' (@ d #{now-864000})
+
+      EOF
+    end
+
+    context "with :include_allowed" do
+      let(:config) { {to: io, include_allowed: true} }
+      it "notifies all into terminal" do
+        out = io.string
+        out.should == <<-EOF
+foo <foo@example.com>:
+  a_spec.rb:15 - pending 'because it fails.' (@ b #{now-864000})
+  a_spec.rb:10 - pending 'because it fails' (@ a #{now-86400})
+
+bar <bar@example.com>:
+  a_spec.rb:25 - pending 'because it fails...' (@ d #{now-864000})
+  a_spec.rb:20 - pending 'because it fails..' (@ c #{now-86400})
+
+        EOF
+      end
+    end
   end
 end
