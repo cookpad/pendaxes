fbb23649c30f139588c35656554e2474585cc736
Shota Fukumori <sorah@cookpad.com>
2012-09-21 13:20:11 +0900

    implement workspace

--- lib/pendaxes/workspace.rb
+++ lib/pendaxes/workspace.rb
@@ -1,9 +1,38 @@
+require_relative 'defaults'
+require 'hashr'
+
 module Pendaxes
   class Workspace
     extend Defaults
 
     def initialize(config={})
-      @config = self.class.defaults.merge(config)
+      @config = Hashr.new(self.class.defaults.merge(config))
+    end
+
+    def clone
+      FileUtils.remove_entry_secure(path) if File.exist?(path)
+      git "clone", @config.repository, path
+    end
+
+    def update
+      clone unless File.exist?(path)
+
+      dive do
+        git "fetch", "origin"
+        git "reset", "--hard", @config.branch || "FETCH_HEAD"
+      end
+    end
+
+    def path; @config.path; end
+
+    def dive
+      Dir.chdir(path) do
+        yield
+      end
+    end
+
+    def git(*args)
+      system(@config.git || "git", *args)
     end
   end
 end
--- spec/workspace_spec.rb
+++ spec/workspace_spec.rb
@@ -1,35 +1,105 @@
 require_relative './spec_helper'
 require 'pendaxes/workspace'
+require 'fileutils'
 
 describe Pendaxes::Workspace do
-  describe "#clone" do
-    it "clones a repository"
+  let(:repository) { "#{File.dirname(__FILE__)}/fixtures/repo" }
+  let(:config) { {path: "/tmp/repo", repository: repository} }
 
-    context "if :directory isn't exist" do
-      it "mkdirs"
+  subject do
+    described_class.new(config)
+  end
+
+  describe "#clone" do
+    it "clones a repository" do
+      File.stub(:exist? => false)
+      subject.should_receive(:git).with("clone", repository, "/tmp/repo")
+
+      subject.clone
     end
 
     context "if repository already cloned" do
-      it "removes then clone again"
+      before do
+        File.stub(:exist? => true)
+      end
+
+      it "removes then clone again" do
+        FileUtils.should_receive(:remove_entry_secure).with("/tmp/repo").ordered
+        subject.should_receive(:git).with("clone", repository, "/tmp/repo")
+        subject.clone
+      end
     end
   end
 
   describe "#update" do
-    it "fetchs"
+    before { File.stub(:exist? => true) }
 
-    it "resets --hard"
+    it "fetch and reset" do
+      subject.should_receive(:dive).and_yield.ordered
+      subject.should_receive(:git).with("fetch", "origin").ordered
+      subject.should_receive(:git).with("reset", "--hard", "FETCH_HEAD").ordered
+
+      subject.update
+    end
+
+    context "if not cloned yet" do
+      before do
+        File.stub(:exist? => false)
+      end
+
+      it "clones first" do
+        subject.should_receive(:clone).ordered
+        subject.should_receive(:dive).and_yield.ordered
+        subject.should_receive(:git).with("fetch", "origin").ordered
+        subject.should_receive(:git).with("reset", "--hard", "FETCH_HEAD").ordered
+
+        subject.update
+      end
+    end
+
+    context "with config.branch" do
+      let(:config) { {directory: "/tmp/repo", repository: repository, branch: "topic"} }
+
+      it "resets to specified branch" do
+        subject.should_receive(:dive).and_yield.ordered
+        subject.should_receive(:git).with("fetch", "origin").ordered
+        subject.should_receive(:git).with("reset", "--hard", "topic").ordered
+
+        subject.update
+      end
+    end
   end
 
   describe "#path" do
-    it "returns the path to working copy"
+    it "returns the path to working copy" do
+      subject.path.should == "/tmp/repo"
+    end
   end
 
   describe "#dive" do
-    it "yields in Dir.chdir"
+    it "yields in Dir.chdir(path)" do
+      Dir.should_receive(:chdir).with(subject.path).and_yield
+
+      flag = false
+      subject.dive { flag = true }
+      flag.should be_true
+    end
   end
 
   describe "#git" do
-    it "invokes git"
+    it "invokes git" do
+      subject.should_receive(:system).with("git", "a", "b", "c").and_return(:foo)
+      subject.git("a", "b", "c").should == :foo
+    end
+
+    context "with config.git" do
+      let(:config) { {directory: "/tmp/repo", repository: "#{File.dirname(__FILE__)}/fixtures/repo", git: "/path/to/git"} }
+
+      it "invokes specified git" do
+        subject.should_receive(:system).with("/path/to/git", "a", "b", "c").and_return(:foo)
+        subject.git("a", "b", "c").should == :foo
+      end
+    end
   end
 end
 
